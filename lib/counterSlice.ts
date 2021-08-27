9072976126b5ce87fd2fab7633323f3f978ef220
Mat Warger <mwarger@gmail.com>
2021-08-27 11:38:25 -0500

    updated counterslice to be the auth machine

--- lib/counterSlice.ts
+++ lib/counterSlice.ts
@@ -1,55 +1,132 @@
-import { assign, createMachine, interpret, sendParent } from "xstate";
+import { assign, createMachine, interpret, Sender } from "xstate";
 import { createXStateSlice } from "./createXStateSlice";
 
 /**
  * Create a machine in the normal XState way, using
  * any feature you like
  */
-const counterMachine = createMachine<{ count: number }>({
-  context: {
-    count: 1,
-  },
-  on: {
-    increment: {
-      actions: assign((context, event) => {
-        return {
-          count: context.count + 1,
-        };
-      }),
-    },
-    decrement: {
-      actions: [
-        assign((context, event) => {
-          return {
-            count: context.count - 1,
-          };
-        }),
-      ],
-    },
-  },
-  invoke: {
-    src: () => (sendBack) => {
-      setInterval(() => {
-        sendBack({
-          type: "decrement",
-        });
-      }, 1000);
-    },
-  },
-});
+
+ export type AuthenticationMachineContext = {
+   userDetails?: UserDetails;
+ };
+ 
+ interface UserDetails {
+   username: string;
+ }
+ 
+ export type AuthenticationMachineEvent =
+   | {
+       type: 'REPORT_IS_LOGGED_IN';
+       userDetails: UserDetails;
+     }
+   | {
+       type: 'REPORT_IS_LOGGED_OUT';
+     }
+   | {
+       type: 'LOG_OUT';
+     }
+   | {
+       type: 'LOG_IN';
+       userDetails: UserDetails;
+     };
+ 
+ const authenticationMachine = createMachine<
+   AuthenticationMachineContext,
+   AuthenticationMachineEvent
+ >(
+   {
+     id: 'authentication',
+     initial: 'checkingIfLoggedIn',
+     states: {
+       checkingIfLoggedIn: {
+         invoke: {
+           src: 'checkIfLoggedIn',
+           onError: {
+             target: 'loggedOut',
+           },
+         },
+         on: {
+           REPORT_IS_LOGGED_IN: {
+             target: 'loggedIn',
+             actions: 'assignUserDetailsToContext',
+           },
+           REPORT_IS_LOGGED_OUT: 'loggedOut',
+         },
+       },
+       loggedIn: {
+         on: {
+           LOG_OUT: {
+             target: 'loggedOut',
+           },
+         },
+       },
+       loggedOut: {
+         entry: ['navigateToAuthPage', 'clearUserDetailsFromContext'],
+         on: {
+           LOG_IN: {
+             target: 'loggedIn',
+             actions: 'assignUserDetailsToContext',
+           },
+         },
+       },
+     },
+   },
+   {
+     services: {
+       checkIfLoggedIn: () => async (
+         send: Sender<AuthenticationMachineEvent>,
+       ) => {
+         const isLoggedIn = true;
+        //  Perform some async check here
+         if (isLoggedIn) {
+           send({
+             type: "REPORT_IS_LOGGED_IN",
+             userDetails: {
+               username: "mpocock1",
+             },
+           });
+         } else {
+           send({
+             type: "REPORT_IS_LOGGED_OUT",
+           });
+         }
+       },
+     },
+     actions: {
+       navigateToAuthPage: () => {
+         // When the user is logged out, we
+         // should take them to the /auth route
+       },
+       assignUserDetailsToContext: assign((context, event) => {
+         if (event.type !== 'REPORT_IS_LOGGED_IN') {
+           return {};
+         }
+         return {
+           userDetails: event.userDetails,
+         };
+       }),
+       clearUserDetailsFromContext: assign((context) => ({
+         userDetails: undefined,
+       })),
+     },
+   },
+ );
+ 
+ export default authenticationMachine;
+ 
 
 export const counterSlice = createXStateSlice({
   // Pass in a unique, descriptive name for the slice
   name: "counter",
   // Pass in the machine
-  machine: counterMachine,
+  machine: authenticationMachine,
   /**
    * Get the state we want to pass from the machine
    * to Redux.
    */
   getSelectedState: (state) => {
     return {
-      count: state.context.count,
+      count: state.context?.userDetails,
     };
   },
 });
